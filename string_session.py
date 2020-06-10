d947663b642586bf0d55eeb971fca21ab8687058
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-06-10 13:53:39 +0530

    Update string_session.py

--- string_session.py
+++ string_session.py
@@ -3,6 +3,7 @@
 # This Source Code Form is subject to the terms of the GNU
 # General Public License, v.3.0. If a copy of the GPL was not distributed with this
 # file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html.
+#credits @spechide
 
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
@@ -12,14 +13,14 @@ Login using your Telegram account
 Click on API Development Tools
 Create a new application, by entering the required details
 
-if Telegram is blocked by your ISP, you can try the @useTGxBot""")
+If Telegram is blocked by your ISP, you can USE VPN and login""")
 API_KEY = int(input("Enter API KEY here: "))
 API_HASH = input("Enter API HASH here: ")
 
 with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
     session_string = client.session.save()
-    saved_messages_template = """@remix 
+    saved_messages_template = """@PPE_Support 
 <code>STRING_SESSION</code>: <code>{}</code>
 
-⚠️ <i>it is forbidden to pass this value to third parties</i>""".format(session_string)
+⚠️ <i>It is forbidden to pass this value to third parties</i>""".format(session_string)
     client.send_message("me", saved_messages_template, parse_mode="html")
