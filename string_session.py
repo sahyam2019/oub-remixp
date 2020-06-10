9655c570abef335a5a94ce8f88c76bdb5b957808
heyworld <57591677+sahyam2019@users.noreply.github.com>
2020-06-10 13:21:18 +0530

    Test

--- string_session.py
+++ string_session.py
@@ -1,18 +1,24 @@
+#!/usr/bin/env python3
+# (c) https://t.me/TelethonChat/37677
+# This Source Code Form is subject to the terms of the GNU
+# General Public License, v.3.0. If a copy of the GPL was not distributed with this
+# file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html.
+
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 
 print("""Please go-to my.telegram.org
 Login using your Telegram account
 Click on API Development Tools
-Create a new application, by entering the required details""")
+Create a new application, by entering the required details
 
-API_KEY = input("API_KEY: ")
-API_HASH = input("API_HASH: ")
+API_KEY = int(input("Enter API KEY here: "))
+API_HASH = input("Enter API HASH here: ")
 
 with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
-    print("Here is your userbot srting, copy it to a safe place !!")
-    print("")
-    print(client.session.save())
-    print("")
-    print("")
-    print("Enjoy your userbot !!")
+    session_string = client.session.save()
+    saved_messages_template = """@remix 
+<code>STRING_SESSION</code>: <code>{}</code>
+
+⚠️ <i>it is forbidden to pass this value to third parties</i>""".format(session_string)
+    client.send_message("me", saved_messages_template, parse_mode="html")
